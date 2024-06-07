f1c14134aa26c915f16178ff9fc971b97c96b58d
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-07 16:23:45 -0300

    Testando exceções (ThrowError)

--- carrinho/test/carrinho.test.js
+++ carrinho/test/carrinho.test.js
@@ -28,4 +28,13 @@ describe('Testes do carrinho', () => {
 
     expect(carrinho).toHaveProperty('total');
   });
+
+  it('Deve lançar um erro ao finalizar compra com carrinho vazio', () => {
+    function englobaErroCarrinho() {
+      const carrinho = new Carrinho();
+      carrinho.finalizaCompra();
+    }
+
+    expect(englobaErroCarrinho).toThrowError('Carrinho de compras vazio');
+  });
 });
