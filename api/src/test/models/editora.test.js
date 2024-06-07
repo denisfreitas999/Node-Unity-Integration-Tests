be536634989e1e8b66315b648a3f7d1454de8854
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-07 20:26:32 -0300

    Criando funções de Dublê (Mocks) para realizar simulações de chamadas ao banco de dados

--- api/src/test/models/editora.test.js
+++ api/src/test/models/editora.test.js
@@ -1,3 +1,4 @@
+import { jest } from '@jest/globals'
 import Editora from "../../models/editora";
 
 describe('Testando o modelo Editora', () => {
@@ -23,7 +24,7 @@ describe('Testando o modelo Editora', () => {
     });
   });
 
-  it('Deve salvar no BD usando a forma assíncrona', async () => {
+  it.skip('Deve salvar no BD usando a forma assíncrona', async () => {
     const editora = new Editora(objetoEditora);
 
     const dados = await editora.salvar();
@@ -39,4 +40,39 @@ describe('Testando o modelo Editora', () => {
       })
     )
   });
+
+  it.skip('Deve fazer uma chamada simulada ao BD simples', () => {
+    const editora = new Editora(objetoEditora);
+
+    editora.salvar = () => {
+      console.log('Editora salva no DB');
+    }
+
+    editora.salvar();
+  });
+
+  it('Deve fazer uma chamada simulada ao BD Mock', () => {
+    const editora = new Editora(objetoEditora);
+
+    editora.salvar = jest.fn().mockReturnValue({
+      id: 5,
+      nome: 'CDC',
+      cidade: 'São Paulo',
+      email: 'c@c.com',
+      created_at: '2024-06-06',
+      updated_at: '2024-06-06'
+    });
+
+    const retorno = editora.salvar();
+
+    expect(retorno).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String)
+      })
+    )
+  });
+
 });
